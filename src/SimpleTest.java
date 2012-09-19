35022ec09a9fb8e2f255828277f231a2f3a0d8ef
Jen Zundel <jenizundel@mac.com>
2012-09-19 12:39:56 -0700

    first commit.

---
+++ src/SimpleTest.java
@@ -0,0 +1,12 @@
+/**
+ * Cambia
+ * User: jen
+ * Date: 9/19/12
+ * Time: 12:31 PM
+ */
+public class SimpleTest {
+
+    public static void main(String[] args) {
+        System.out.println("This is a sample system out println.");
+    }
+}
